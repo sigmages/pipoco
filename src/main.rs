c12df8023c2da696d31ac9839003b7c590ac937c
Ronald Rodrigues Farias <ronald-farias@outlook.com>
2022-12-12 23:33:36 -0300

    refactor: add /sticker cmd

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
+use std::borrow::Borrow;
+use std::fs;
+
 use frankenstein::GetUpdatesParams;
 use frankenstein::SendMessageParams;
+use frankenstein::SendStickerParams;
 use frankenstein::TelegramApi;
 use frankenstein::{Api, UpdateContent};
 use rand::Rng;
 
-static TOKEN: &str = "";
-
+static TOKEN: &str = "5922619577:AAHGRGbHTcYonxmQEQbR7MMeEVZa57p_0rY";
+fn build_sticker(chat_id: i64) -> Vec<SendStickerParams> {
+    let base_path = "./bichostrostes/png";
+    let dir = fs::read_dir(&base_path).unwrap();
+    let size = dir.count();
+    let mut dir = fs::read_dir(&base_path).unwrap();
+    let mut rng = rand::thread_rng();
+    let n = rng.gen_range(0..size);
+    let filename = dir.nth(n).expect("should return a file").unwrap().file_name();
+    let filename = filename.to_str().unwrap();
+    let sticker = format!("{}/{}", base_path, filename);
+    let file = std::path::PathBuf::from(sticker.clone());
+    let message = SendStickerParams::builder().chat_id(chat_id).sticker(file).build();
+    vec![message]
+}
 fn build_messages(chat_id: i64) -> Vec<SendMessageParams> {
     let mut rng = rand::thread_rng();
     let rows = rng.gen_range(1..5);
@@ -49,6 +66,13 @@ fn send_messages(messages: &Vec<SendMessageParams>, api: &Api) {
         }
     }
 }
+fn send_stickers(messages: &Vec<SendStickerParams>, api: &Api) {
+    for message in messages {
+        if let Err(err) = api.send_sticker(message) {
+            println!("Failed to send message: {:?}", err);
+        }
+    }
+}
 fn main() {
     let api = Api::new(TOKEN);
 
@@ -64,11 +88,12 @@ fn main() {
             Ok(response) => {
                 for update in response.result {
                     if let UpdateContent::Message(message) = update.content {
-                        if message.text.unwrap_or("".to_string()) == "/acende" {
+                        if message.text.clone().unwrap_or("".to_string()) == "/acende" {
                             let messages = build_messages(message.chat.id);
                             send_messages(&messages, &api);
-                        } else {
-
+                        } else if message.text.clone().unwrap_or("".to_string()) == "/sticker" {
+                            let messages = build_sticker(message.chat.id);
+                            send_stickers(&messages, &api);
                         }
                     }
                     update_params = update_params_builder
